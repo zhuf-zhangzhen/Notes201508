ee6e17366a5f137dc217f570bb31d713079a97e0
zhouxiaotian <1282347298@qq.com>
2016-01-31 19:10:20 +0800

    20160131_周末版第九周第二天-轮播图、音视频、本地存储...

--- 20160131_周末班第九周第二天/banner/js/index.js
+++ 20160131_周末班第九周第二天/banner/js/index.js
@@ -70,17 +70,95 @@
         }
     }
 
-    /*
-     按照JS传统的轮播图实现的方式，我们发现一个问题：我们让运动的时间设置为0s后，立马又设置为0.5s，浏览器还没有反应过来就变为0.5s了，所以我们会出现到最后一张后，从后到前的运动到第二张
-     function autoMove() {
-     step++;
-     if (step > count + 1) {
-     inner.style.webkitTransitionDuration = "0s";
-     inner.style.left = -winW + "px";
-     step = 2;
-     }
-     inner.style.webkitTransitionDuration = "0.5s";
-     inner.style.left = -step * winW + "px";
-     selectTip();
-     }*/
+    //5、实现左右滑动
+    ["start", "move", "end"].forEach(function (item) {
+        inner.addEventListener("touch" + item, eval(item), false);
+    });
+
+    function start(e) {
+        //console.dir(e);//->TouchEvent:changedTouches、targetTouches、touches(TouchList->包含了当前手的位置信息)
+
+        //->滑动开始:禁止自动轮播,取消当前元素的过渡效果
+        window.clearInterval(autoTimer);
+        this.style.webkitTransitionDuration = "0s";
+
+        //->记录当前元素的开始的坐标和left值
+        var touchPoint = e.touches[0];
+        this["strX"] = touchPoint.pageX;
+        this["strY"] = touchPoint.pageY;
+        this["strL"] = parseFloat(this.style.left);
+    }
+
+    function move(e) {
+        //->获取最新的坐标
+        var touchPoint = e.touches[0];
+        this["endX"] = touchPoint.pageX;
+        this["endY"] = touchPoint.pageY;
+
+        //->判断是否发生滑动，并且获取滑动的方向
+        this["swipeFlag"] = isSwipe(this["strX"], this["endX"], this["strY"], this["endY"]);
+        //->说明发生了滑动
+        if (this["swipeFlag"]) {
+            this["swipeDir"] = swipeDirection(this["strX"], this["endX"], this["strY"], this["endY"]);
+            //->只有左右滑动才操作
+            if (/^(Right|Left)$/.test(this["swipeDir"])) {
+
+                //->计算滑动的距离,并且让当前的元素的left跟着改变
+                this["changeX"] = this["endX"] - this["strX"];
+                this.style.left = this["strL"] + this["changeX"] + "px";
+            }
+        }
+    }
+
+    function end(e) {
+        //->结束的时候判断是否发生滑动
+        if (this["swipeFlag"]) {
+            if (this["swipeDir"] === "Left") {
+                if (Math.abs(this["changeX"]) >= (winW / 4)) {
+                    step++;
+                }
+            }
+            if (this["swipeDir"] === "Right") {
+                if (Math.abs(this["changeX"]) >= (winW / 4)) {
+                    step--;
+                }
+            }
+        }
+        this.style.webkitTransitionDuration = "0.5s";
+        this.style.left = -step * winW + "px";
+        selectTip();
+
+        //->滑动的边界判断:当我们滑动到最后一张的时候(0.5s),我们让当前的inner立马回到step=1的时候的位置
+        var _this = this;
+        if (step > count) {
+            window.setTimeout(function () {
+                _this.style.webkitTransitionDuration = "0s";
+                _this.style.left = -winW + "px";
+                step = 1;
+            }, 500);
+        }
+        if (step < 1) {
+            window.setTimeout(function () {
+                _this.style.webkitTransitionDuration = "0s";
+                _this.style.left = -count * winW + "px";
+                step = count;
+            }, 500);
+        }
+
+        //->开启自动轮播,并且把之前设置的那些自定义属性的值赋值为null
+        autoTimer = window.setInterval(autoMove, autoInterval);
+        ["strX", "strY", "strL", "endX", "endY", "swipeDir", "swipeFlag", "changeX"].forEach(function (item) {
+            _this[item] = null;
+        });
+    }
+
+    function isSwipe(strX, endX, strY, endY) {
+        return Math.abs(endX - strX) > 30 || Math.abs(endY - strY) > 30;
+    }
+
+    function swipeDirection(strX, endX, strY, endY) {
+        var changeX = endX - strX;
+        var changeY = endY - strY;
+        return Math.abs(changeX) > Math.abs(changeY) ? (changeX > 0 ? "Right" : "Left") : (changeY > 0 ? "Down" : "Up");
+    }
 })();
